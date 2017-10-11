4f544f58c4cfc603c3dae3d9731b64a9cfed56a8
sezen <sezen@rocketmail.com>
2017-10-11 11:19:33 +0200

    servlet

--- seating/src/main/java/com/project/java/seating/SeatingServlet.java
+++ seating/src/main/java/com/project/java/seating/SeatingServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class SeatingServlet
  */
-@WebServlet({ "/seating" })
+@WebServlet({ "/" })
 public class SeatingServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
