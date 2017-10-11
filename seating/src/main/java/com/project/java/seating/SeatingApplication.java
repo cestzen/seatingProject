109d7d969095f9d0bf93a1bce06795a2e8d6314e
Sylvain <Sylvain@leblay>
2017-10-11 10:19:26 +0200

    initial commit

---
+++ seating/src/main/java/com/project/java/seating/SeatingApplication.java
@@ -0,0 +1,12 @@
+package com.project.java.seating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatingApplication.class, args);
+	}
+}
