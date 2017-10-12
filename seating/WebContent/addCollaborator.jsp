96e64de871e3e7e4381f038bc1c408c6dfbd8895
sezen <sezen@rocketmail.com>
2017-10-12 16:33:55 +0200

    add coll

--- seating/WebContent/addCollaborator.jsp
+++ seating/WebContent/addCollaborator.jsp
@@ -3,7 +3,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <body>
-<form action="./" method="POST">
+<form action="./ajoutCollab" method="POST">
 Prenom: <input type="text" name="name">
 <br />
 Nom: <input type="text" name="lastname" />
--- seating/src/main/java/com/project/java/seating/servlet/SeatingServlet.java
+++ seating/src/main/java/com/project/java/seating/servlet/SeatingServlet.java
@@ -51,11 +51,7 @@ public class SeatingServlet extends HttpServlet{
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
-		addCollaborateurService = new AddCollaborateurService();
-		addCollaborateurService.addCollaborateur(request.getParameter("lastname"),request.getParameter("name"));
-		response.getWriter().append(request.getParameter("lastname")).append(request.getParameter("name"));
+		doGet(request, response);
 	}
 
 	
