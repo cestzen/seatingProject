278b4522bf7aabaadc15c05aa5b1de8ba02b1bc3
beril <beril@10.133.129.2>
2017-10-12 09:00:45 +0200

    service collab

---
+++ seating/src/main/java/com/project/java/seating/services/AddCollaborateurService.java
@@ -0,0 +1,20 @@
+package com.project.java.seating.services;
+
+import java.util.Date;
+
+import org.hibernate.jpa.criteria.expression.function.CurrentDateFunction;
+
+import com.project.java.seating.model.Collaborateur;
+import com.project.java.seating.persistence.ProjectEntityManager;
+
+public class AddCollaborateurService {
+
+	public void addCollaborateur(String Nom, String Prenom) {
+		ProjectEntityManager projectEntityManager = new ProjectEntityManager();
+		Collaborateur collaborateur = new Collaborateur();
+		collaborateur.setNom_collaborateur("Baykut");
+		collaborateur.setPrenom_collaborateur("Beril");
+		collaborateur.setEstAdministrateur(true);
+		collaborateur.setDateArriver(new Date().toString());
+	}		
+}
